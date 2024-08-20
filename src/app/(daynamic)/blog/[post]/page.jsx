ee9f4fd94f53cd2c4ac3045052547b096899b257
My Name <hishamrehab309@gmail.com>
2024-08-20 07:35:54 +0300

    Products Page

--- src/app/(daynamic)/blog/[post]/page.jsx
+++ src/app/(daynamic)/blog/[post]/page.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styles from "./page.module.css"
+import Image from "next/image"
+import blogIamge from "/public/pexels-photo-261579.jpeg"
 
 export default function Post() {
     return (
-        <div>Post</div>
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <div className={styles.info}>
+                    <h1 className={styles.title}>Hello Next JS</h1>
+                    <p className={styles.desc}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti non, esse reprehenderit delectus culpa repellat provident labore vero enim, officiis ab, earum accusamus mollitia dicta voluptatum consequuntur perferendis accusantium rerum.</p>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image src={blogIamge} alt="post image" fill={true} className={styles.postImage} />
+                    <span className={styles.author}>Hisham Rehab</span>
+                </div>
+
+            </header>
+            <div className={styles.content}>
+                <p className={styles.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae corrupti, unde commodi, eaque itaque animi natus dolorum dolores placeat reprehenderit sunt fugit non sapiente quia deleniti nulla possimus assumenda repellendus!   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ipsa adipisci sapiente nesciunt repudiandae quo, in minima ad praesentium eius ullam laboriosam maiores quasi totam possimus sunt dolor deleniti provident.   Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis rem cupiditate tempora porro soluta labore perspiciatis odio expedita itaque animi ut, quam sint architecto laboriosam aspernatur, nesciunt excepturi nam mollitia!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem explicabo, minima cumque voluptas maxime facere laborum debitis! Ratione velit adipisci laboriosam accusamus tenetur? Exercitationem non iure quam et nobis minus.   Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam aut voluptates, accusantium eius, ab corporis quia sit autem vero aliquam quo recusandae aspernatur veritatis porro corrupti facilis vel ipsam.  </p>
+            </div>
+        </div>
     )
 }
--- src/app/(daynamic)/blog/page.jsx
+++ src/app/(daynamic)/blog/page.jsx
@@ -1,7 +1,32 @@
-
-
+import Link from "next/link";
+import BlogImage from "/public/pexels-photo-261579.jpeg";
+import Image from "next/image";
+import styles from "./page.module.css"
 export default function Blog() {
     return (
-        <div>Page</div>
+        <div className={styles.mainContainer}>
+
+            <Link href="blog/id" className={styles.post}>
+                <div className={styles.container}>
+                    <Image src={BlogImage} width={350} height={250} alt="post image" />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Post Title</h1>
+                    <p className={styles.text}>Post Text</p>
+                </div>
+            </Link>
+
+
+            <Link href="/id" className={styles.post}>
+                <div className={styles.container}>
+                    <Image src={BlogImage} width={350} height={250} alt="post image" />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Post Title</h1>
+                    <p className={styles.text}>Post Text</p>
+                </div>
+            </Link>
+
+        </div >
     )
 }
